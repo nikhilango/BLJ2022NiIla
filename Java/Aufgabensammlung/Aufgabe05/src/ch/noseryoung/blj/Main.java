9f97bc97456e4a50ae79f0d43f24a1d627bad6a2
nikhilango <nikhil.ilango@noseryoung.com>
2022-11-10 09:46:09 +0100

    added Aufgabe05

---
+++ Java/Aufgabensammlung/Aufgabe05/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,12 @@
+package ch.noseryoung.blj;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for( int i = 3; i <= 27 ;i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
