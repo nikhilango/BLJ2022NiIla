5ea4546f7c5f098e8da76125f4b9a62e50384ad5
nikhilango <nikhil.ilango@noseryoung.com>
2022-11-30 16:02:29 +0100

    added the decorate functions

--- Java/KW48/Aquarium/src/ch/noseryoung/blj/Main.java
+++ Java/KW48/Aquarium/src/ch/noseryoung/blj/Main.java
@@ -92,19 +92,19 @@ public class Main {
             System.out.println();
         }
 
-        System.out.println("****************AQUARIUM DATA****************");
+        System.out.println("*************************AQUARIUM DATA*************************");
         for (int i = 0; i < names.size(); i++){
             System.out.println("    -" + names.get(i) + " is a type: " + fishtypes.get(i) + " is at the x-position " + xpos.get(i) + " and at the y-position " + ypos.get(i));
         }
-        System.out.println("******************MOVE FISH********************");
+        System.out.println("***************************MOVE FISH*****************************");
         System.out.print("Do you want to move your fishes? 1/0: ");
         int mode = input.nextInt();
         if (mode == 1){
             System.out.print("Select a fish: ");
             String changeFish = input1.nextLine();
-            System.out.print("Select x-position: ");
+            System.out.print("Move to x-position: ");
             int changex = input2.nextInt();
-            System.out.print("Select y-position: ");
+            System.out.print("Move to y-position: ");
             int changey = input3.nextInt();
             if (tank[changey - 1][changex - 1].equals(" ▢ ")){
                 int i;
@@ -115,16 +115,36 @@ public class Main {
                          break;
                      }
                  }
-                System.out.println(i);
-                 int prevx = xpos.get(i);
-                System.out.println(prevx);
-                int prevy = ypos.get(i);
-                System.out.println(prevy);
-                 tank[prevy - 1][prevx - 1] = " ▢ ";
+                 tank[ypos.get(i) - 1][xpos.get(i) - 1] = " ▢ ";
             }
             for (int i = 0; i < tanksize; i++){
                 for (int j = 0; j < tanksize; j++){
-                    System.out.print(tank[i][j] + " | ");
+                    System.out.print(tank[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("*****************************DECORATE*******************************");
+        System.out.print("Do you want to add decorations? 1/0: ");
+        int mode2 = input.nextInt();
+        if (mode2 == 1){
+            System.out.println("Select a x-position: ");
+            int decx = input.nextInt();
+            System.out.println("Select a y-position: ");
+            int decy = input1.nextInt();
+            System.out.println("What do you want to add to you aquarium? (1: rock/ 2: shell) : ");
+            int decoration = input2.nextInt();
+            switch (decoration){
+                case 1:
+                    tank[decy - 1][decx - 1] = "\uD83E\uDEA8";
+                    break;
+                case 2:
+                    tank[decy - 1][decx - 1] = "\uD83D\uDC1A";
+                    break;
+            }
+            for (int i = 0; i < tanksize; i++){
+                for (int j = 0; j < tanksize; j++){
+                    System.out.print(tank[i][j]);
                 }
                 System.out.println();
             }
