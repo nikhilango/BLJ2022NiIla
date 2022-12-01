b40adc547638c1ec0acfaa6dc8710cdc7fb677e6
nikhilango <nikhil.ilango@noseryoung.com>
2022-12-01 16:36:05 +0100

    added Aufgabe 15

--- Java/Aufgabensammlung/Aufgabe14/untitled/src/ch/noseryoung/blj/Main.java
+++ Java/Aufgabensammlung/Aufgabe14/untitled/src/ch/noseryoung/blj/Main.java
@@ -3,15 +3,15 @@ package ch.noseryoung.blj;
 public class Main {
     public static void main(String[] args) {
         int numbers[] = new int[] { 1, 5, 3, 19, 29, 25 };
-        int diff = Integer.MAX_VALUE;
+        int difference = Integer.MAX_VALUE;
 
         for (int i = 0; i < numbers.length - 1; i++){
             for (int j = i + 1; j < numbers.length; j++){
-                if (Math.abs((numbers[i] - numbers[j])) < diff){
-                    diff = Math.abs((numbers[i] - numbers[j]));
+                if (Math.abs((numbers[i] - numbers[j])) < difference){
+                    difference = Math.abs((numbers[i] - numbers[j]));
                 }
             }
         }
-        System.out.println("Minimum difference is " + diff);
+        System.out.println("Minimum difference is " + difference);
     }
 }
