befbb852af3129ade0cd4a0955d6282bf892357b
nikhilango <nikhil.ilango@noseryoung.com>
2022-12-22 14:11:34 +0100

    added GameOfLife

---
+++ Java/KW51/GameOfLife/Starter.java
@@ -0,0 +1,13 @@
+/**
+ * The starter class. Clean, simple and short, as all starter classes should be.
+ * 
+ * @author surber
+ *
+ */
+public class Starter {
+  public static void main(String[] args) {
+    Simulation sim = new Simulation(100, true, Preset.PULSAR_17X17);
+    SimulationPanel simPanel = new SimulationPanel(sim, 40, 0.9);
+    simPanel.getSimulation().start();
+  }
+}
