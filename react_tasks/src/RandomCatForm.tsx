1554cfd45b3edbcd1cb1814a3ccb42edd65fbe83
nikhilango <nikhil.ilango@noseryoung.com>
2023-04-12 14:15:18 +0200

    added new files for task 3

---
+++ react_tasks/src/RandomCatForm.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from "@mui/material";
+
+function RandomCatForm(){
+    return(
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h4" component="h2">
+                Form here
+              </Typography>
+            </Grid>
+          </Grid>
+    );
+}
