829be14c75c1d6cfe7553f20759e8b8482cdb3e5
nikhilango <nikhil.ilango@noseryoung.com>
2023-07-05 15:28:12 +0200

    finished minimum functionalities

--- Java/KW27/Starter.java
+++
@@ -1,8 +0,0 @@
-package ch.noseryoung;
-
-public class Starter {
-  public static void main(String[] args) {
-    Console console = new Console();
-    console.start();
-  }
-}
---
+++ Java/KW27/timedquiz/Timer.java
@@ -0,0 +1,24 @@
+package ch.noseryoung;
+
+public class Timer extends Thread{
+    private QuizManager manager;
+    public Timer(QuizManager manager){
+        this.manager = manager;
+    }
+    @Override
+    public void run(){
+        System.out.println("it started");
+        try {
+            Thread.sleep(3000);
+            manager.showQuestion2();
+            System.out.println("question 2 displayed");
+            Thread.sleep(3000);
+            manager.showQuestion3();
+            System.out.println("question 2 displayed");
+            Thread.sleep(3000);
+            System.out.println("it has stopped");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
