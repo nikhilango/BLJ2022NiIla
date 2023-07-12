a104c9269801ea2db91b2f141c854bb1a65b913a
nikhilango <nikhil.ilango@noseryoung.com>
2023-07-12 11:23:45 +0200

    added snake game

---
+++ Java/KW28/Snake/src/ch/noseryoung/game/GameThread.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.game;
+
+public class GameThread extends Thread{
+    private Game game;
+
+    public GameThread(Game game){
+        this.game = game;
+    }
+    @Override
+    public void run(){
+
+    }
+}
